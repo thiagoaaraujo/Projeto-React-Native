3d178b2932325b042bec2e6bb57df115aa55b82a
Thiago Aparecido Araújo <thiagoaaraujo@unipam.edu.br>
2023-07-26 17:36:13 -0300

    Lista de itens

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import mock from './src/mocks/cesta';
+import AppLoading from 'expo-app-loading';
 
 
 export default function App() {
@@ -10,13 +13,13 @@ export default function App() {
   })
 
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1}}>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
--- orgs-cesta/src/telas/Cesta/componentes/Detalhes.js
+++ orgs-cesta/src/telas/Cesta/componentes/Detalhes.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
 import { Texto } from "../../../componentes/Texto";
-import logo from "../../../../assets/logo.png";
+import Botao from "./Botao";
 
-export default function Detalhes() {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
     return <>
-        <Texto style={estilos.nome}>Cesta de verduras</Texto>
+        <Texto style={estilos.nome}>{nome}</Texto>
         <View style={estilos.fazenda}>
-            <Image source={logo} style={estilos.imagemFazenda} />
-            <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+            <Image source={logoFazenda} style={estilos.imagemFazenda} />
+            <Texto style={estilos.nomeFazenda}>{nomeFazenda}</Texto>
         </View>
 
-        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda para sua cozinha.</Texto>
-        <Texto style={estilos.preco}>R$ 40,00</Texto>
+        <Texto style={estilos.descricao}>{descricao}</Texto>
+        <Texto style={estilos.preco}>{preco}</Texto>
+        <Botao texto={botao} style={estilos.botao} onPress={() => {}} />
     </>
 }
 
@@ -54,5 +55,8 @@ const estilos = StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         marginTop: 8
-    }
+    },
+    botao: {
+        marginTop: 16,
+    },
 })
